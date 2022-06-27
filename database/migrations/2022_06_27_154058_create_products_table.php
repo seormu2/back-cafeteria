4db3ceac4402b21b3f1c5bcf3b100feda6a9d759
sebastian <alex.delgado@ias.com.co>
2022-06-27 10:52:45 -0500

    creacion de la tabla productos

---
+++ database/migrations/2022_06_27_154058_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nameProduct', 100);
+            $table->string('referency', 100);
+            $table->integer('price');
+            $table->integer('weight');
+            $table->string('category', 100);
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
